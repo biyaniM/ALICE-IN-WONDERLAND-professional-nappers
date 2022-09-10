b6e3eb59a990d82891a9509382fbfc1dc6e55fb2
Yanjie <suyanjie36@gmail.com>
2022-09-09 23:55:38 -0700

    Created UI of HUD components: Health Bar, Timer and Ammunition Scores

---
+++ Assets/HUD/HealthBar/Script/HealthBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//ref: https://www.youtube.com/watch?v=BLfNP4Sc_iA&t=518s
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
---
+++ Assets/HUD/Timer/Script/CountDownTimer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+//source: https://www.youtube.com/watch?v=2gPHkaPGbpI
+//clickalbe to pause
+public class CountDownTimer : MonoBehaviour , IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Pause = !Pause;
+    }
+
+    [SerializeField] private Image uiFill;
+    [SerializeField] private Text uiText;
+
+    public int Duration;
+
+    private int remainingDuration;
+    
+    private bool Pause;
+
+    private void Start()
+    {
+        Begin(Duration);
+    }
+
+    private void Begin(int Second)
+    {
+        remainingDuration = Second;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while(remainingDuration >= 0)
+        {
+            if (!Pause)
+            {
+                uiText.text = $"{remainingDuration / 60:00}:{remainingDuration % 60:00}";
+                uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
+                remainingDuration--;
+                yield return new WaitForSeconds(1f);
+            }
+            yield return null;
+        }
+        OnEnd();
+    }
+
+    private void OnEnd()
+    {
+        //End Time , if want Do something
+        print("End");
+    }
+}
