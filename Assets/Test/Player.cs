335ae49742e0f1384c3c1aa5e18069c487042226
Yanjie <suyanjie36@gmail.com>
2022-09-11 16:49:16 -0700

    Updated Ammunition Component: press space key to increase scores

--- Assets/Test/Player.cs
+++ Assets/Test/Player.cs
@@ -7,11 +7,21 @@ public class Player : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
+    public AmmunitionScore ammunitionScore;
+    public int startScore = 0;
+    public int redScore;
+    public int greenScore;
+    public int blueScore;
     // Start is called before the first frame update
     void Start()
     {
+        redScore = startScore;
+        greenScore = startScore;
+        blueScore = startScore;
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        ammunitionScore.SetScores(redScore, greenScore, blueScore);
+        
     }
 
     // Update is called once per frame
@@ -20,6 +30,8 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(20);
+            GetScores();
+
         }
         
     }
@@ -28,5 +40,9 @@ public class Player : MonoBehaviour
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+    }
+
+    void GetScores(){
+        ammunitionScore.SetScores(++redScore, ++greenScore, ++blueScore);
     }
 }
