6b9d5f614fc3e4938edcfa6b73abb43faf77f7ac
Yanjie <suyanjie36@gmail.com>
2022-09-11 16:49:16 -0700

    Updated Ammunition Component: press space key to increase scores

--- Assets/HUD/Ammunition/Script/AmmunitionScore.cs
+++ Assets/HUD/Ammunition/Script/AmmunitionScore.cs
@@ -1,19 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AmmunitionScore : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] public Text redScore;
+    [SerializeField] public Text greenScore;
+    [SerializeField] public Text blueScore;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetScores(int red, int green, int blue){
+        redScore.text = red.ToString();
+        greenScore.text = green.ToString();
+        blueScore.text = blue.ToString();
     }
 }
