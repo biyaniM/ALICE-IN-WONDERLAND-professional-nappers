441df201e4b7ad582393d9edfb9db6b43d4b2a03
shrishtiag02 <97569171+shrishtiag02@users.noreply.github.com>
2022-09-12 16:47:46 -0700

    Player Movement Added
    Movements(L,R,F,B) and Jump.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveZ = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+
+        moveDirection = new Vector3(moveX, 0, moveZ);
+
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+            moveDirection *= moveSpeed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Jump
+                Jump();
+            }
+        }
+        
+        controller.Move(moveDirection * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+    }
+
+    private void Jump()
+    {
+        Idle();
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+}
