88c1c507603c6af8d72b1bfdb2565b50fcd2783f
kanish99-usc <kanishni@usc.edu>
2022-09-13 13:00:27 -0700

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,45 +7,45 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     CharacterController ch;
 
-    void Start() {
-        ch = GetComponent<CharacterController>();
-    }
+    // void Start() {
+    //     ch = GetComponent<CharacterController>();
+    // }
 
-    void update()
+    // void update()
+    // {
+    //     float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+    //     float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+    //     ch.Move(new Vector3(x,0,z));
+    // }
+    private bool jumpKeyWasPressed;
+
+    private float horizontalInput;
+
+    private Rigidbody rigidbodyComponent;
+    // Start is called before the first frame update
+    void Start()
     {
-        float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        ch.Move(new Vector3(x,0,z));
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
-    // private bool jumpKeyWasPressed;
 
-    // private float horizontalInput;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
 
-    // private Rigidbody rigidbodyComponent;
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     rigidbodyComponent = GetComponent<Rigidbody>();
-    // }
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         jumpKeyWasPressed = true;
-    //     }
+    private void FixedUpdate(){
 
-    //     horizontalInput = Input.GetAxis("Horizontal");
-    // }
+        if (jumpKeyWasPressed){
+            rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
 
-    // private void FixedUpdate(){
-
-    //     if (jumpKeyWasPressed){
-    //         rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
-    //         jumpKeyWasPressed = false;
-    //     }
-
-    //     rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 1);
-    // }
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 1);
+    }
 }
