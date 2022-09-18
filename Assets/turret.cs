5786793eb6691c49ef1e4ae923186871570e093a
kanish99-usc <kanishni@usc.edu>
2022-09-18 16:21:25 -0700

    Enemy Movement acc to Player and set Range

--- Assets/turret.cs
+++ Assets/turret.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class turret : MonoBehaviour
 {
-    [SerializeField] float turretRange = 13f;
+    [SerializeField] float turretRange = 0.0002f;
     [SerializeField] float turretRotationSpeed = 0.05f;
 
     private Transform playerTransform;  //could be changed to 'target' 
     private gun currentGun;
     private float fireRate;
     private float fireRateDelta;
+    public GameObject player;
 
     private void Start()
     {
+       playerTransform = player.transform;
+       Debug.Log(playerTransform);
         // playerTransform = FindObjectOfType<PlayerArmature>().transform;   
                         //could replace this with a public function that sets target
                         //On Trigger Enter if there is multiple targets
@@ -23,27 +26,31 @@ public class turret : MonoBehaviour
 
     private void Update()
     {
-        // Vector3 playerGroundPos = new Vector3(playerTransform.position.x, 
-        //                           transform.position.y, playerTransform.position.z);
+        Vector3 playerGroundPos = new Vector3(playerTransform.position.x, 
+                                  transform.position.y, playerTransform.position.z);
+        Debug.Log("Player ground pos====");
+        Debug.Log(playerGroundPos);
+        Debug.Log(Vector3.Distance(transform.position, playerGroundPos));
 
-        // //Check if player is not in range, then do nothing
-        // if(Vector3.Distance(transform.position, playerGroundPos) > turretRange)
-        // {
-        //     return; //do nothing because player is not in range
-        // }
+        //Check if player is not in range, then do nothing
+        if(Vector3.Distance(transform.position, playerGroundPos) > turretRange)
+        {
+            
+            return; //do nothing because player is not in range
+        }
 
-        //PLAYER IN RANGE
+        // PLAYER IN RANGE
 
-        //Rotate Turret towards player
-       // Vector3 playerDirection = playerGroundPos - transform.position;
-        transform.Rotate(new Vector3(0f,0.3f,0f));
+        // Rotate Turret towards player
+       Vector3 playerDirection = playerGroundPos - transform.position;
+      //  transform.Rotate(new Vector3(0f,0.3f,0f));
         float turretRotationStep = turretRotationSpeed * Time.deltaTime;
-        // Vector3 newLookDirection = Vector3.RotateTowards(transform.forward, playerDirection,
-                                 //  turretRotationStep, 0f);
-       // transform.rotation = Quaternion.LookRotation(newLookDirection);
+        Vector3 newLookDirection = Vector3.RotateTowards(transform.forward, playerDirection,
+                                  turretRotationStep, 0f);
+       transform.rotation = Quaternion.LookRotation(newLookDirection);
 
         fireRateDelta -= 2*Time.deltaTime;
-        if(fireRateDelta <= 0)
+        if(fireRateDelta <= 0 && Vector3.Distance(transform.position, playerGroundPos) < turretRange)
         {
             currentGun.Fire();
             currentGun.Fire();
@@ -58,4 +65,12 @@ public class turret : MonoBehaviour
     {
         Gizmos.DrawWireSphere(transform.position, turretRange); //Show a gizmo when selected
     }
+
+    // private void OnCollisionEnter(Collision collision) {
+    //     if(collision.transform.tag== "Enemy ")
+    //     {
+    //         Destroy(collision.gameObject);
+    //         gameObject.SetActive(false);
+    //     }
+    // }
 }
