8c9f79165c0a1bc9954bb83abb06899989102162
Kushal Agarwal <97569163+kushalagar@users.noreply.github.com>
2022-09-19 02:42:36 -0700

    Added Intitial Analytics tracking metrics
    Added initial metrics to track level started, level completed, and numbers of bullets used. Added an Analytics script to send relevant statistics to a google form for plotting graphs.

--- Assets/Scripts/LevelComplete/LevelComplete.cs
+++ Assets/Scripts/LevelComplete/LevelComplete.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LevelComplete : MonoBehaviour
 {
     public bool levelOverCheck = false;
+    int ammoBalance;
+    int i = 1;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +22,18 @@ public class LevelComplete : MonoBehaviour
         
     }
 
+    
+
     public void OnTriggerEnter(Collider col){
         if(col.gameObject.tag == "LevelCompleteTag"){
-            Debug.Log("reached level over loc!"); 
+            Debug.Log("reached level over loc!");
+            Debug.Log(i);
+            i = i + 1;
             levelOverCheck = true;
-
+            ThirdPersonShooterController shootingComponent = GetComponent<ThirdPersonShooterController>();
+            ammoBalance = shootingComponent.currentAmmo;
+            SendToGoogle analyticsComponent = GetComponent<SendToGoogle>();
+            analyticsComponent.Send("1", ammoBalance.ToString(), "0");
             //! show the level complete HUD
 
         }
