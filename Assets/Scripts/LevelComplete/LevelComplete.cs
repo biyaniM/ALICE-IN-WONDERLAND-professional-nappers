1725f9b3c84b2c4b95db5f27d1248db40b74b55b
Gautam Pranjal <gautam17pranjal@gmail.com>
2022-09-19 12:46:46 -0700

    Update LevelComplete.cs

--- Assets/Scripts/LevelComplete/LevelComplete.cs
+++ Assets/Scripts/LevelComplete/LevelComplete.cs
@@ -7,12 +7,13 @@ public class LevelComplete : MonoBehaviour
 {
     public bool levelOverCheck = false;
     int ammoBalance;
-    
+    ThirdPersonShooterController shootingComponent;
+    SendToGoogle analyticsComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,18 +22,26 @@ public class LevelComplete : MonoBehaviour
         
     }
 
+    void Awake(){
+        shootingComponent = GetComponent<ThirdPersonShooterController>();
+        analyticsComponent = GetComponent<SendToGoogle>();
+    }
     
 
     public void OnTriggerEnter(Collider col){
-        if(col.gameObject.tag == "LevelCompleteTag"){
-            Debug.Log("reached level over loc!");
-            levelOverCheck = true;
-            ThirdPersonShooterController shootingComponent = GetComponent<ThirdPersonShooterController>();
-            ammoBalance = shootingComponent.currentAmmo;
-            SendToGoogle analyticsComponent = GetComponent<SendToGoogle>();
-            analyticsComponent.Send("1", ammoBalance.ToString(), "0");
-            //! show the level complete HUD
+        Debug.Log("collider: "+col.name);
+        // get ammo balance
+        ammoBalance = shootingComponent.currentAmmo;
+        if(levelOverCheck == false){
+            if(col.gameObject.tag == "LevelCompleteTag"){
+                Debug.Log("reached level over loc!");
+                levelOverCheck = true;
+                // capture the analytics
+                analyticsComponent.Send("1", ammoBalance.ToString(), "0");
 
+                //! show the level complete HUD
+
+            }
         }
     }
 }
