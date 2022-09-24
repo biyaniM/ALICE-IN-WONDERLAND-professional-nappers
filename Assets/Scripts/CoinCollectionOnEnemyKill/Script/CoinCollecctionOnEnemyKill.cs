d45da2264d30618cce1ba392e95df9dbeaf6ed15
Yanjie <suyanjie36@gmail.com>
2022-09-23 23:46:01 -0400

    Connected enemykill to scoreboard and refactored scoreboard updating methods

--- Assets/Scripts/CoinCollectionOnEnemyKill/Script/CoinCollecctionOnEnemyKill.cs
+++ Assets/Scripts/CoinCollectionOnEnemyKill/Script/CoinCollecctionOnEnemyKill.cs
@@ -9,6 +9,7 @@ public class CoinCollecctionOnEnemyKill : MonoBehaviour
     public int blueCoins;
     public int yellowCoins;
     private bool passed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,7 @@ public class CoinCollecctionOnEnemyKill : MonoBehaviour
     void Awake(){
         // redCoins = int.Parse(player.coinsScore.redScore.text);
         // blueCoins = int.Parse(player.coinsScore.blueScore.text);
-        // yellowCoins = int.Parse(player.coinsScore.yellowScore.text);
+         // yellowCoins = int.Parse(player.coinsScore.yellowScore.text);
         passed = false;
     }
 
@@ -28,19 +29,29 @@ public class CoinCollecctionOnEnemyKill : MonoBehaviour
                 Debug.Log("Passed?"+passed);
                 passed = true;
                 Debug.Log("Hit!");
+                redCoins = player.GetRedCoinsScore();
+                blueCoins = player.GetBlueCoinsScore();
+                yellowCoins = player.GetYellowCoinsScore();
+
                 if (gameObject.tag=="enemy1"){
                     Debug.Log("Before Red Kill"+redCoins);
                     redCoins += 1;
+                    player.UpdateCoins(redCoins, blueCoins, yellowCoins);
                     Debug.Log("After Red Kill"+redCoins);
                 }
                 else if (gameObject.tag=="enemy2"){
                     // Debug.Log("Before Blue Kill"+redCoins);
-                    blueCoins += 1;
+                    yellowCoins += 1;
+                    player.UpdateCoins(redCoins, blueCoins, yellowCoins);
+
+                    
                     // Debug.Log("After Red Kill"+redCoins);
                 }
                 else if (gameObject.tag=="enemy3"){
                     // Debug.Log("Before Red Kill"+redCoins);
-                    yellowCoins += 1;
+                    blueCoins += 1;
+                    player.UpdateCoins(redCoins, blueCoins, yellowCoins);
+                    
                     // Debug.Log("After Red Kill"+redCoins);
                 }
                 else{
@@ -52,9 +63,7 @@ public class CoinCollecctionOnEnemyKill : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Debug.Log("EnemyKillRED"+redCoins);
-        player.UpdateCoins(redCoins, blueCoins, yellowCoins);
+    {        
          //check if player has collected enough colors to pass gate
         player.CheckGoal(redCoins, blueCoins, yellowCoins);
     }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -86,9 +86,9 @@ public class Player : MonoBehaviour
     void InitializeHUD(){
         healthPoint = SUM_HEALTH;
         ammoBalance = SUM_AMMO;
-        redCoins = RED_GOAL;
-        blueCoins = BLUE_GOAL;
-        yellowCoins = YELLOW_GOAL;
+        redCoins = 0;
+        blueCoins = 0;
+        yellowCoins = 0;
         Debug.Log("Initialize HUD values");
     }
 
@@ -108,5 +108,17 @@ public class Player : MonoBehaviour
         finishBoundary.GetComponent<BoxCollider>().enabled = param;
     }
 
+
+    public int GetRedCoinsScore(){
+        return redCoins;
+    }
+
+    public int GetBlueCoinsScore(){
+        return blueCoins;
+    }
+
+    public int GetYellowCoinsScore(){
+        return yellowCoins;
+    }
 
 }
