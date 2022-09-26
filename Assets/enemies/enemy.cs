3e6c58c6983f39ef87c0350bcaab82cc0ef7fc69
Manan Biyani <mananbiyani2209@outlook.com>
2022-09-26 15:02:14 -0700

    changed turrent movement speed and extra rotation

--- Assets/enemies/enemy.cs
+++ Assets/enemies/enemy.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     [SerializeField] float turretRange = 13f;
-    [SerializeField] int turretRotationOnDetectionSpeed = 10;
-    [SerializeField] float angleThreshold = 15.0f;
+    [SerializeField] int turretRotationOnDetectionSpeed = 30;
+    [SerializeField] float angleThreshold = 60.0f;
     [SerializeField] int turretShootRange = 7;
 
    // private Transform playerTransform;  //could be changed to 'target' 
@@ -45,14 +45,18 @@ public class enemy : MonoBehaviour
             //* Rotate if the player is in range for the turret
             Quaternion rotation = Quaternion.LookRotation(playerDirection);
             Quaternion current = transform.localRotation;
-            transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * turretRotationOnDetectionSpeed);
+            transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * turretRotationOnDetectionSpeed * 1.5f);
             
             //* Angle function checks the angle between the players position vector and enemy's position vector
-            if (180-Vector3.Angle(currentGun.transform.forward,playerTarget.transform.position)<=angleThreshold){
+            float angle = 180 - Vector3.Angle(currentGun.transform.forward,playerTarget.transform.position);
+            if (angle<=angleThreshold){
                 //* If the enemy is in a certain angle threshold, only then shoot
                 currentGun.Fire();
                 fireRateDelta = fireRate;
-            } 
+                rotation = Quaternion.LookRotation(playerDirection);
+                current = transform.localRotation;
+                transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * turretRotationOnDetectionSpeed);
+            }
         }
        
     }
