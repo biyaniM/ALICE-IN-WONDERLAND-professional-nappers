05314bc77cda6e1c341cd0584495e359f9736dba
kanish99-usc <kanishni@usc.edu>
2022-09-28 16:31:26 -0700

    Added Horizontal to-n-fro enemy movement
    Co-Authored-By: Astha Mishra <52097499+ASTHAMISHRA4@users.noreply.github.com>

--- Assets/enemy_to_and_fro.cs
+++ Assets/enemy_to_and_fro.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class enemy_to_and_fro : enemy
 {
-    
+    public float speed = 2.5f;
+    void Update()
+    {
+        base.Update();
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 2), transform.position.y, transform.position.z);
+    }
 
 }
