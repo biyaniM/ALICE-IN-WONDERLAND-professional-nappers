b198a7a6eb7d5a3b8e746227db722fb39bd286b6
Gautam Pranjal <gautam17pranjal@gmail.com>
2022-10-04 13:37:01 -0700

    coauthor adding
    Co-Authored-By: Manan Biyani <39940797+biyaniM@users.noreply.github.com>
    Co-Authored-By: schsu1 <29050624+schsu1@users.noreply.github.com>
    Co-Authored-By: shouvik-ghosh <70974532+shouvik-ghosh@users.noreply.github.com>
    Co-Authored-By: Shrishti Agarwal  <97569171+shrishtiag02@users.noreply.github.com>
    Co-Authored-By: siddhantbagga15 <97336499+siddhantbagga15@users.noreply.github.com>
    Co-Authored-By: Yanjie Su <32280657+sulysu@users.noreply.github.com>
    Co-Authored-By: kanish99-usc <112135552+kanish99-usc@users.noreply.github.com>
    Co-Authored-By: Kushal Agarwal <97569163+kushalagar@users.noreply.github.com>
    Co-Authored-By: Astha Mishra <52097499+ASTHAMISHRA4@users.noreply.github.com>

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; 
 
 public class Player : MonoBehaviour
 {
