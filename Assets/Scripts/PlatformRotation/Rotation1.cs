3dad09695cb29b5abd33301d21644c6d63b7058c
schsu1 <schsu@usc.edu>
2022-10-09 16:00:56 -0700

    sub level 2 complete
    added the up down movement
    clockwise + anti clockwise rotation
    
    Co-Authored-By: Yanjie Su <32280657+sulysu@users.noreply.github.com>
    Co-Authored-By: Gautam Pranjal <17716635+gautam17pranjal@users.noreply.github.com>
    Co-Authored-By: shouvik-ghosh <70974532+shouvik-ghosh@users.noreply.github.com>

--- Assets/Scripts/PlatformRotation/Rotation1.cs
+++ Assets/Scripts/PlatformRotation/Rotation1.cs
@@ -8,12 +8,19 @@ public class Rotation1 : MonoBehaviour
     [SerializeField] private Vector3 _rotation;
     [SerializeField] private float _speed; 
 
+    [SerializeField] private bool rotation_clockwise;
+
 
     // Use this for initialization
     void Start()
     {
         _speed = 20;
-        StartCoroutine(RotateObject(90, Vector3.up, 3));
+        if (rotation_clockwise){
+            StartCoroutine(RotateObject(90, Vector3.up, 3));
+        }
+        else{
+            StartCoroutine(RotateObject(90, Vector3.down, 3));
+        }
     }
 
     IEnumerator RotateObject(float angle, Vector3 axis, float inTime)
@@ -31,12 +38,12 @@ public class Rotation1 : MonoBehaviour
                 deltaAngle += _speed* Time.deltaTime;
                 deltaAngle = Mathf.Min(deltaAngle, angle);
 
-                transform.RotateAround(transform.position, Vector3.up, _speed*Time.deltaTime);
-                Debug.Log("---------------------");
-                Debug.Log(transform.position.x);
-                Debug.Log(transform.position.y);
-                Debug.Log(transform.position.z);
-                Debug.Log("---------------------");
+                transform.RotateAround(transform.position, axis, _speed*Time.deltaTime);
+                // Debug.Log("---------------------");
+                // Debug.Log(transform.position.x);
+                // Debug.Log(transform.position.y);
+                // Debug.Log(transform.position.z);
+                // Debug.Log("---------------------");
                 // transform.Rotate(_rotation * Time.deltaTime);
 
                 yield return null;
