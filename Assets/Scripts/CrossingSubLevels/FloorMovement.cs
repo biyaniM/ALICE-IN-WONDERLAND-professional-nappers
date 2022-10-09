aff43de16e2891bf0fae51f385577354de232c5a
schsu1 <schsu@usc.edu>
2022-10-09 16:00:56 -0700

    sub level 2 complete
    added the up down movement
    clockwise + anti clockwise rotation
    
    Co-Authored-By: Yanjie Su <32280657+sulysu@users.noreply.github.com>
    Co-Authored-By: Gautam Pranjal <17716635+gautam17pranjal@users.noreply.github.com>
    Co-Authored-By: shouvik-ghosh <70974532+shouvik-ghosh@users.noreply.github.com>

--- Assets/Scripts/CrossingSubLevels/FloorMovement.cs
+++ Assets/Scripts/CrossingSubLevels/FloorMovement.cs
@@ -1,33 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class FloorMovement : MonoBehaviour
-// {
-//     public float speed = 5f;
-//     [SerializeField] private Vector3 curr_pos;
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         // pingPongBetweenLevels();
-//         curr_pos = new Vector3(transform.position.x, 0, 0);
-//         Debug.Log("curr_pos-----");
-//         Debug.Log(curr_pos);
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         transform.position = curr_pos + new Vector3(Mathf.PingPong(Time.time * speed, 18), transform.position.y, transform.position.z);
-//         // transformww.position = new Vector3(Mathf.PingPong(-1, 8), transform.position.y, transform.position.z);
-        
-//     }
-
-// //     void pingPongBetweenLevels(){
-// //         transform.position = new Vector3(Mathf.Sin(Mathf.PingPong(Time.time, 2* Mathf.PI)));
-// //     }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
