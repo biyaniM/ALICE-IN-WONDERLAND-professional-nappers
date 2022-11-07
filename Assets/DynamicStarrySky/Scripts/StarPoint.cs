fc423df87122345d254ccd105de1d5e43a46002c
Gautam Pranjal <gautam17pranjal@gmail.com>
2022-11-06 17:26:57 -0800

    added assets
    Co-Authored-By: schsu1 <29050624+schsu1@users.noreply.github.com>

---
+++ Assets/DynamicStarrySky/Scripts/StarPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Data for a star in the star system.
+public class StarPoint : System.Object {
+  public Vector3 position;
+  public float noise;
+  public float xRotation;
+  public float yRotation;
+
+  public StarPoint(Vector3 position, float noise, float xRotation, float yRotation)
+  {
+    this.position = position;
+    this.noise = noise;
+    this.xRotation = xRotation;
+    this.yRotation = yRotation;
+  }
+}
