c44f8b3b09aee2f810447efdf2f127268f03e0e5
Yanjie <suyanjie36@gmail.com>
2022-11-11 14:12:28 -0800

    Added new menu system

---
+++ Assets/SlimUI/Editor/SlimUIWindow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SlimUIWindow : EditorWindow {
+
+	//string myString = "Hello";
+
+	[MenuItem("Window/SlimUI/Online Documentation")]
+	public static void ShowWindow(){
+		//GetWindow<SlimUIWindow>("Example");
+		Application.OpenURL("https://www.slimui.com/documentation");
+	}
+/* UNDER CONSTRUCTION - Coming in later update
+	void OnGUI(){
+		GUILayout.Label("SlimUI Control Panel", EditorStyles.boldLabel);
+
+		myString = EditorGUILayout.TextField("Name", myString);
+	}
+*/
+}
---
+++ Assets/SlimUI/Vivid/Scripts/FillToText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SlimUI.Vivid{
+	public class FillToText : MonoBehaviour {
+		public TMP_Text fillText;
+		string fillValue;
+		public Image fillImage;
+		
+		// for demonstration purposes only
+		void Update(){
+			UpdateText();
+		}
+
+		public void UpdateText(){
+			fillValue = Mathf.Round(fillImage.fillAmount * 100).ToString();
+			fillText.text = fillValue;
+		}
+	}
+}
