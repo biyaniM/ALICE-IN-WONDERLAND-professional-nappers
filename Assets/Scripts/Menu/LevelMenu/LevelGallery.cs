055a34d48970dae1a0d5e8b6aa79fa1869bdf89a
Yanjie <suyanjie36@gmail.com>
2022-11-13 17:52:19 -0800

    Linked brief items to gallery in level menu

--- Assets/Scripts/Menu/LevelMenu/LevelGallery.cs
+++ Assets/Scripts/Menu/LevelMenu/LevelGallery.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class LevelGallery : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler{
+public class LevelGallery : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler{
     public RectTransform levelPreview;
+    public GameObject levelBrief;
  
     Vector3 cachedScale;
  
@@ -17,13 +18,19 @@ public class LevelGallery : MonoBehaviour,IPointerEnterHandler, IPointerExitHand
     public void OnPointerEnter(PointerEventData eventData) {
          
         levelPreview.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        levelBrief.SetActive(true);
         Debug.Log("Hover In!");
 
     }
  
     public void OnPointerExit(PointerEventData eventData) {
- 
         levelPreview.localScale = cachedScale;
+        levelBrief.SetActive(false);
         Debug.Log("Hover Out");
+    }
+
+    public void OnPointerClick(PointerEventData eventData){
+        //go to level
+        
     }
  }
