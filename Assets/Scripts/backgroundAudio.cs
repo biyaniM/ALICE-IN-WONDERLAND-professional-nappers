0070f2fd3cbb5f603f57f725879a749d6ef15df7
siddhantbagga15 <siddhantbagga123@gmail.com>
2022-11-26 22:07:48 -0800

    audio mute

--- Assets/Scripts/backgroundAudio.cs
+++ Assets/Scripts/backgroundAudio.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class backgroundMusic : MonoBehaviour
+public class backgroundAudio : MonoBehaviour
 {   
     AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        muteAudio();
     }
 
     public void muteAudio() {
