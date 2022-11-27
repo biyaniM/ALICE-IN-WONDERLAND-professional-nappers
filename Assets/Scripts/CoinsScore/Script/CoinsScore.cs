01606f3d734f163edde0c1215c0a8d3e67b28c98
Yanjie <suyanjie36@gmail.com>
2022-11-26 22:42:25 -0800

    Change score color based on goal

--- Assets/Scripts/CoinsScore/Script/CoinsScore.cs
+++ Assets/Scripts/CoinsScore/Script/CoinsScore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,14 @@ public class CoinsScore : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] public Text totalScore;
     [SerializeField] public Text Goal;
+    private int gemGoal;
     private bool isTwoDigitsGoal;
 
     public void SetScores(int coins){
+        coins = Math.Min(coins,gemGoal);
+        if(coins == gemGoal){
+            totalScore.color = new Color32(255, 36, 0, 242);
+        }
         if(isTwoDigitsGoal && coins < 10){
             totalScore.text = "0" + coins.ToString();
         }else{
@@ -20,6 +26,7 @@ public class CoinsScore : MonoBehaviour
     }
 
     public void SetGoals(int goal){
+        gemGoal = goal;
         if(goal >= 10){
             isTwoDigitsGoal = true;
         }
