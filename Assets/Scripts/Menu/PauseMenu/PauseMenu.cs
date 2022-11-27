0eaf36fc800308e0fe7a45e2c04f50d5e7cfb974
Manan Biyani <39940797+biyaniM@users.noreply.github.com>
2022-11-26 17:33:32 -0800

    Pause Menu Overhaul (#267)
    * pause system overhaul
    
    * Pause Input Bug #246 with home and reset btn fix
    
    * debug log clear
    
    * resume and home button in pause menu shooting fix
    
    * build setting upd

--- Assets/Scripts/Menu/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu/PauseMenu.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using StarterAssets;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] public bool GameIsPaused = false;
+    // [SerializeField] public bool GameIsPaused = false;
     public Button resume_btn;
     public Button menu_btn;
     public Button restart_btn;
     public Button guide_btn;
     public CountDownTimer timer;
+    private ThirdPersonController controller;
+
+    void Awake(){
+        controller = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonController>();
+    }
 
     private void Start() {
         gameObject.SetActive(false);
     }
 
     public void Setup(){    // pause the game
-        Debug.Log("Pause Menu Set");
         gameObject.SetActive(true);
         //release the cursor to press the button.
         Cursor.lockState = CursorLockMode.None;
@@ -28,7 +33,6 @@ public class PauseMenu : MonoBehaviour
         //guide_btn.onClick.AddListener(ShowGuideMenu);
         // timer.pauseTimer();
         Time.timeScale = 0f;
-        GameIsPaused = true;
         Cursor.visible = true;
     }
     
@@ -37,8 +41,9 @@ public class PauseMenu : MonoBehaviour
         // timer.resumeTimer();
         gameObject.SetActive(false);
         Time.timeScale = 1f;
-        GameIsPaused = false;
+        // GameIsPaused = false;
         Cursor.visible = false;
+        controller.SetPause(false);
     }
 
     void Restart(){     // working
@@ -46,6 +51,7 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Cursor.visible = false;
+        controller.SetPause(false);
     }
 
     void BackToMenu(){  // working
@@ -53,6 +59,7 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         //SceneManager.LoadScene("Menu");
         SceneManager.LoadScene("New_Menu");
+        controller.SetPause(false);
     }
 
     void ShowGuideMenu(){
