bf912056ba8a92ae8796fd5f5cd835b504e52ded
Yanjie <suyanjie36@gmail.com>
2022-11-27 00:10:41 -0800

    Resolved rebase conflict

--- Assets/Scripts/CoinsScore/Script/CoinsScore.cs
+++ Assets/Scripts/CoinsScore/Script/CoinsScore.cs
@@ -8,12 +8,21 @@ public class CoinsScore : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] public Text totalScore;
     [SerializeField] public Text Goal;
+    private bool isTwoDigitsGoal;
 
     public void SetScores(int coins){
-        totalScore.text = coins.ToString();
+        if(isTwoDigitsGoal && coins < 10){
+            totalScore.text = "0" + coins.ToString();
+        }else{
+            totalScore.text = coins.ToString();
+        }
+        
     }
 
     public void SetGoals(int goal){
+        if(goal >= 10){
+            isTwoDigitsGoal = true;
+        }
         Goal.text = goal.ToString();
     }
 }
